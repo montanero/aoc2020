295b702df908a514c0c7d5992767913caddf5299
Roland Bergler <roland@bergler.net>
2020-12-02 07:03:25 +0100

    day02a

---
+++ day02/day02.go
@@ -0,0 +1,43 @@
+package day02
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+
+	"bergler.net/reader"
+)
+
+func Result(file string) (int, error) {
+	pattern := regexp.MustCompile(`^(\d+)-(\d+) ([a-z]): ([a-z]+)$`)
+	count := 0
+	if data, err := reader.ReadStrings(file); err == nil {
+		for _, d := range data {
+
+			groups := pattern.FindStringSubmatch(d)
+			i0, e0 := strconv.Atoi(groups[1])
+			_ = e0
+			i1, e1 := strconv.Atoi(groups[2])
+			_ = e1
+			c := []rune(groups[3])[0]
+			passwd := groups[4]
+			if check(i0, i1, c, passwd) {
+
+				count = count + 1
+			}
+		}
+		return count, nil
+	}
+	return 0, errors.New("problem")
+}
+
+func check(min int, max int, c rune, val string) bool {
+	count := 0
+	for _, d := range val {
+		if d == c {
+			count++
+		}
+	}
+	return count >= min && count <= max
+
+}
---
+++ day02/day02_test.go
@@ -0,0 +1,21 @@
+package day02
+
+import "testing"
+
+func TestResult(t *testing.T) {
+	r, err := Result("data/test1.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	if r != 2 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", r, 2)
+	}
+
+	r2, err2 := Result("data/input.txt")
+	if err2 != nil {
+		t.Error(err2)
+	}
+	if r2 != 469 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", r2, 469)
+	}
+}
