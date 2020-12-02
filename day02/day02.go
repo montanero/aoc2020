a738a94c979946f843b196ccc1f22b4dda5486ee
Roland Bergler <roland@bergler.net>
2020-12-02 07:08:15 +0100

    day02b

--- day02/day02.go
+++ day02/day02.go
@@ -41,3 +41,33 @@ func check(min int, max int, c rune, val string) bool {
 	return count >= min && count <= max
 
 }
+
+func ResultB(file string) (int, error) {
+	pattern := regexp.MustCompile(`^(\d+)-(\d+) ([a-z]): ([a-z]+)$`)
+	count := 0
+	if data, err := reader.ReadStrings(file); err == nil {
+		for _, d := range data {
+
+			groups := pattern.FindStringSubmatch(d)
+			i0, e0 := strconv.Atoi(groups[1])
+			_ = e0
+			i1, e1 := strconv.Atoi(groups[2])
+			_ = e1
+			c := []rune(groups[3])[0]
+			passwd := groups[4]
+			if check2(i0, i1, c, passwd) {
+
+				count = count + 1
+			}
+		}
+		return count, nil
+	}
+	return 0, errors.New("problem")
+}
+
+func check2(pos0 int, pos1 int, c rune, val string) bool {
+	runes := []rune(val)
+	b0 := runes[pos0-1] == c
+	b1 := runes[pos1-1] == c
+	return b0 && !b1 || b1 && !b0
+}
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -19,3 +19,21 @@ func TestResult(t *testing.T) {
 		t.Errorf("Result was incorrect, got: %d, want: %d.", r2, 469)
 	}
 }
+
+func TestResultB(t *testing.T) {
+	r, err := ResultB("data/test1.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	if r != 1 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", r, 1)
+	}
+
+	r2, err2 := ResultB("data/input.txt")
+	if err2 != nil {
+		t.Error(err2)
+	}
+	if r2 != 267 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", r2, 267)
+	}
+}
