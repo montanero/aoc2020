410b445241243dad916ac58aaaaa89887dde74b0
Roland Bergler <roland@bergler.net>
2020-12-15 21:43:44 +0100

    day14b

--- ts/src/day14.test.ts
+++ ts/src/day14.test.ts
@@ -1,6 +1,9 @@
-import { resultA } from './day14';
+import { resultA,resultB } from './day14';
 
 test('result A ', () => {
   expect(resultA("data/day14/input.txt")).toBe(2346881602152);
 }); 
 
+test('result B', () => {
+  expect(resultB("data/day14/input.txt")).toBe(3885232834169);
+}); 
--- ts/src/day14.ts
+++ ts/src/day14.ts
@@ -13,25 +13,12 @@ abstract class InstructionBase {
 
 class InstructionMask extends InstructionBase {
   icode: Icode = Icode.MASK
-  andMask: bigint
-  orMaske: bigint
+  mask: string
 
   constructor(mask: string) {
     super()
-    let a = 0n
-    let o = 0n
-    for (const c of mask) {
-      a *= 2n
-      o *= 2n
-      switch (c) {
-        case '1': o |= 1n; break
-        case '0': a |= 1n; break
-      }
-    }
-    this.andMask = a
-    this.orMaske = o
+    this.mask = mask
   }
-
 }
 
 class InstructionMem extends InstructionBase {
@@ -82,6 +69,21 @@ class Machine {
     this.pc = 0
   }
 
+  getBitMasks(mask: string) {
+    let a = 0n
+    let o = 0n
+    for (const c of mask) {
+      a *= 2n
+      o *= 2n
+      switch (c) {
+        case '1': o |= 1n; break
+        case '0': a |= 1n; break
+      }
+    }
+    return { "and": a, "or": o }
+  }
+
+
   executeSingle(instr: InstructionBase): void {
     switch (instr.icode) {
       case Icode.MASK:
@@ -89,7 +91,8 @@ class Machine {
         break
       case Icode.MEM:
         const imem = instr as InstructionMem
-        let val = (BigInt(imem.val) | this.mask!.orMaske) & ~this.mask!.andMask
+        const masks = this.getBitMasks(this.mask!.mask)
+        let val = (BigInt(imem.val) | masks["or"]) & ~masks["and"]
         this.mem.set(imem.addr, Number(val))
         break
       default:
@@ -103,10 +106,82 @@ class Machine {
   }
 }
 
+class MachineV2 {
+  code: InstructionBase[]
+  pc: number
+  mask: InstructionMask | null = null
+  mem: Map<bigint, number> = new Map()
+
+  constructor(code: InstructionBase[]) {
+    this.code = code
+    this.pc = 0
+  }
+
+  getAddresses(addr: bigint, mask: string): bigint[] {
+    let nand = 0n
+    let or = 0n
+    let floats: bigint[] = [0n]
+    let bit = 1n << BigInt(mask.length - 1)
+    for (const c of mask) {
+      switch (c) {
+        case '1':
+          or |= bit
+          break
+        case '0':
+          break
+        case 'X':
+          nand |= bit;
+          let addfloats: bigint[] = []
+          for (const f of floats) {
+            addfloats.push(f | bit)
+          }
+          floats.push(...addfloats)
+          break
+        default:
+          throw "haeh"
+      }
+      bit >>= 1n
+    }
+    return floats.map(m => (addr & ~nand) | or | m)
+  }
+
+
+  executeSingle(instr: InstructionBase): void {
+    switch (instr.icode) {
+      case Icode.MASK:
+        this.mask = instr as InstructionMask
+        break
+      case Icode.MEM:
+        const imem = instr as InstructionMem
+        for (const addr of this.getAddresses(BigInt(imem.addr), this.mask!.mask)) {
+          this.mem.set(addr, imem.val)
+        }
+        break
+      default:
+        throw "illegal instruction"
+    }
+  }
+  execute(instr: InstructionBase[]): void {
+    for (let i of instr) {
+      this.executeSingle(i)
+    }
+  }
+}
 
 export function resultA(fileName: string): number {
   let program = parse(fileName)
   let m = new Machine(program)
+  m.execute(m.code)
+  let sum = 0
+  for (const v of m.mem.values()) {
+    sum += v
+  }
+  return sum
+}
+
+export function resultB(fileName: string): number {
+  let program = parse(fileName)
+  let m = new MachineV2(program)
   m.execute(m.code)
   let sum = 0
   for (const v of m.mem.values()) {
