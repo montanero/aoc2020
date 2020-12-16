947e52c808a218e1ff26d1149ffef4d67244bf09
Roland Bergler <roland@bergler.net>
2020-12-16 08:09:25 +0100

    day15b

--- ts/src/day15.test.ts
+++ ts/src/day15.test.ts
@@ -1,4 +1,4 @@
-import { resultA } from './day15';
+import { resultA, resultB } from './day15';
 
 test('result A test ', () => {
   expect(resultA("0,3,6")).toBe(436);
@@ -8,3 +8,10 @@ test('result A', () => {
   expect(resultA("0,13,1,16,6,17")).toBe(234);
 }); 
 
+test('result B test ', () => {
+  expect(resultB("0,3,6", 2020)).toBe(436);
+}); 
+
+test('result B', () => {
+  expect(resultB("0,13,1,16,6,17",  30000000)).toBe(8984);
+}); 
--- ts/src/day15.ts
+++ ts/src/day15.ts
@@ -23,8 +23,34 @@ export function resultA(input: string): number {
       numbers.push(0)
     }
     else {
-      numbers.push(numbers.length-1 - index)
+      numbers.push(numbers.length - 1 - index)
     }
   }
   return numbers.pop()!
+}
+
+
+export function resultB(input: string, count:number): number {
+  let numbers = parseInput(input)
+  let lastpos: Map<number, number> = new Map()
+  let i
+  for (i = 0; i < numbers.length-1; i++) {
+    lastpos.set(numbers[i], i)
+  }
+    let last=  numbers[numbers.length-1]
+
+  for (; i < count-1; i++) {
+    const x = lastpos.get(last)
+    let next:number
+    if (x !== undefined){
+       next = i-x
+    }
+    else{
+      next = 0
+    }
+    lastpos.set(last, i)
+    last = next
+  }
+
+  return last
 }
