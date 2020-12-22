59d3d9e565f90ea4517679ee6658ac50bb49aa6e
Roland Bergler <roland@bergler.net>
2020-12-22 17:15:57 +0100

    day22b

--- ts/src/day22.test.ts
+++ ts/src/day22.test.ts
@@ -1,5 +1,13 @@
-import { resultA } from './day22';
+import { resultA, resultB } from './day22';
 
 test('result A', () => {
-    expect(resultA("data/day22/input.txt")).toBe(18482479935793);
+    expect(resultA("data/day22/input.txt")).toBe(30197);
+});
+
+test('test B', () => {
+    expect(resultB("data/day22/test.txt")).toBe(291);
+});
+
+test('result B', () => {
+    expect(resultB("data/day22/input.txt")).toBe(34031);
 });
--- ts/src/day22.ts
+++ ts/src/day22.ts
@@ -18,6 +18,21 @@ class Deck {
         }
         return ret
     }
+
+    seen: Set<string> = new Set()
+
+    isDuplicate(): boolean {
+        let s = this.cards.map(c => "" + c).join(",")
+        if (this.seen.has(s)) {
+            return true
+        }
+        this.seen.add(s)
+        return false
+    }
+
+    copy(n:number): Deck {
+        return new Deck(this.player, [...this.cards].slice(0,n))
+    }
 }
 
 function readDecks(fileName: string): Deck[] {
@@ -53,4 +68,44 @@ export function resultA(fileName: string): number {
         return deck1.score()
 
     return deck2.score()
+}
+
+export function resultB(fileName: string): number {
+    let decks = readDecks(fileName)
+    let deck1 = decks[0]
+    let deck2 = decks[1]
+
+    let winner = playRecursive(deck1, deck2)
+    if (winner == 1) {
+        return deck1.score()
+    }
+    return deck2.score()
+}
+
+function playRecursive(deck1: Deck, deck2: Deck): number {
+
+    while (deck1.cards.length > 0 && deck2.cards.length > 0) {
+        if (deck1.isDuplicate() || deck2.isDuplicate()) {
+            return 1
+        }
+
+        const card1 = deck1.cards.shift()!
+        const card2 = deck2.cards.shift()!
+        let winner: number
+        if (deck1.cards.length >= card1 && deck2.cards.length >= card2) {
+            winner = playRecursive(deck1.copy(card1), deck2.copy(card2))
+        } else {
+            winner = card1 > card2 ? 1 : 2
+        }
+        if (winner == 1) {
+            deck1.cards.push(card1, card2)
+        } else {
+            deck2.cards.push(card2, card1)
+        }
+    }
+    if (deck1.cards.length === 0) {
+        return 2
+    } else {
+        return 1
+    }
 }
