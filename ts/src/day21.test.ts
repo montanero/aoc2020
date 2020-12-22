f0caa87cc1e23fe40d4049d6cbc38b6934a83030
Roland Bergler <roland@bergler.net>
2020-12-22 20:33:37 +0100

    day21b

--- ts/src/day21.test.ts
+++ ts/src/day21.test.ts
@@ -1,4 +1,4 @@
-import { resultA} from './day21';
+import {resultA, resultB} from './day21';
 
 test('test A', () => {
     expect(resultA("data/day21/test.txt")).toBe(5);
@@ -6,4 +6,12 @@ test('test A', () => {
 
 test('result A', () => {
     expect(resultA("data/day21/input.txt")).toBe(1913);
+});
+
+test('test B', () => {
+    expect(resultB("data/day21/test.txt")).toBe("mxmxvkd,sqjhc,fvjkl");
+});
+
+test('result B', () => {
+    expect(resultB("data/day21/input.txt")).toBe("gpgrb,tjlz,gtjmd,spbxz,pfdkkzp,xcfpc,txzv,znqbr");
 });
--- ts/src/day21.ts
+++ ts/src/day21.ts
@@ -42,3 +42,28 @@ export function resultA(fileName: string): number {
 
     return rules.map(r=>r.ingredients).map(is=>intersect(is, safeIngredients).size).reduce((a,b)=>a+b)
 }
+
+export function resultB(fileName: string): string {
+    let rules = readList(fileName)
+    let allergens = rules.map(r => r.allergens).reduce((a, b) => union(a, b))
+    let ingredients = rules.map(r => r.ingredients).reduce((a, b) => union(a, b))
+    let possibly: Map<string, Set<string>> = new Map()
+    for (const allergene of allergens) {
+        const possiblyContains = rules.filter(r => r.allergens.has(allergene)).map(r => r.ingredients).reduce((a, b) => intersect(a, b))
+        possibly.set(allergene, possiblyContains)
+    }
+
+    let known: Map<string, string> = new Map()
+
+    while (possibly.size > 0) {
+        [...possibly].filter(([k, v]) => v.size == 1).forEach(([allergen, v]) => {
+            const ingredient = [...v][0]
+            possibly.delete(allergen)
+            known.set(allergen, ingredient);
+            [...possibly.values()].forEach(ingreds => {
+                ingreds.delete(ingredient)
+            })
+        })
+    }
+    return [...allergens].sort((a, b) => a < b ? -1 : 1).map(a => known.get(a)).join(",")
+}
